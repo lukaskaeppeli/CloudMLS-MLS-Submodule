2032160fa05012fff2ecf5274f424d6533b8fbbc
Hubert Chathi <hubert@uhoreg.ca>
2020-11-03 23:21:46 -0500

    add the constants file

---
+++ src/constants.ts
@@ -0,0 +1,102 @@
+/*
+Copyright 2020 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// various constants
+
+import {stringToUint8Array} from "./util";
+
+export const EMPTY_BYTE_ARRAY = new Uint8Array(0);
+
+// Uint8Array versions of strings
+export const CANDIDATE = stringToUint8Array("candidate");
+export const DKP_PRK = stringToUint8Array("dkp_prk");
+export const EAE_PRK = stringToUint8Array("eae_prk");
+export const EXP = stringToUint8Array("exp");
+export const HPKE = stringToUint8Array("HPKE");
+export const INFO_HASH = stringToUint8Array("info_hash");
+export const KEY = stringToUint8Array("key");
+export const MLS10 = stringToUint8Array("mls10 ");
+export const NODE = stringToUint8Array("node");
+export const NONCE = stringToUint8Array("nonce");
+export const PATH = stringToUint8Array("path");
+export const PSK_HASH = stringToUint8Array("psk_hash");
+export const PSK_ID_HASH = stringToUint8Array("psk_id_hash");
+export const SEC = stringToUint8Array("sec");
+export const SECRET = stringToUint8Array("secret");
+export const SHARED_SECRET = stringToUint8Array("shared_secret");
+export const SK = stringToUint8Array("sk");
+
+
+// uint16
+export enum CipherSuite {
+    MLS10_128_DHKEMX25519_AES128GCM_SHA256_Ed25519 = 1,
+    MLS10_128_DHKEMP256_AES128GCM_SHA256_P256 = 2,
+    MLS10_128_DHKEMX25519_CHACHA20POLY1305_SHA256_Ed25519 = 3,
+    MLS10_256_DHKEMX448_AES256GCM_SHA512_Ed448 = 4,
+    MLS10_256_DHKEMP521_AES256GCM_SHA512_P521 = 5,
+    MLS10_256_DHKEMX448_CHACHA20POLY1305_SHA512_Ed448 = 6,
+}
+
+// uint16
+export enum ExtensionType {
+    Capabilities = 1,
+    Lifetime = 2,
+    KeyId = 3,
+    ParentHash = 4,
+    RatchetTree = 5,
+}
+
+// uint16
+export enum CredentialType {
+    Basic = 1,
+    X509 = 2,
+}
+
+// See RFC 8446 and the IANA TLS SignatureScheme registry
+export enum SignatureScheme {
+    rsa_pkcs1_sha256 = 0x0401,
+    rsa_pkcs1_sha384 = 0x0501,
+    rsa_pkcs1_sha512 = 0x0601,
+
+    /* ECDSA algorithms */
+    ecdsa_secp256r1_sha256 = 0x0403,
+    ecdsa_secp384r1_sha384 = 0x0503,
+    ecdsa_secp521r1_sha512 = 0x0603,
+
+    /* RSASSA-PSS algorithms with public key OID rsaEncryption */
+    rsa_pss_rsae_sha256 = 0x0804,
+    rsa_pss_rsae_sha384 = 0x0805,
+    rsa_pss_rsae_sha512 = 0x0806,
+
+    /* EdDSA algorithms */
+    ed25519 = 0x0807,
+    ed448 = 0x0808,
+
+    /* RSASSA-PSS algorithms with public key OID RSASSA-PSS */
+    rsa_pss_pss_sha256 = 0x0809,
+    rsa_pss_pss_sha384 = 0x080a,
+    rsa_pss_pss_sha512 = 0x080b,
+
+    /* Legacy algorithms */
+    rsa_pkcs1_sha1 = 0x0201,
+    ecdsa_sha1 = 0x0203,
+}
+
+// uint8
+export enum ProtocolVersion {
+    Reserved = 0,
+    Mls10 = 1,
+}
